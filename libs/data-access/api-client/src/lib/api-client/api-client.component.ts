a0c43ad28c309d56087cf7f5e4b983998b0ce9f4
aniketpandey2912 <aniketpandey2912@gmail.com>
2025-06-28 19:00:12 +0530

    feat: apps and packages

---
+++ libs/data-access/api-client/src/lib/api-client/api-client.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-api-client',
+  imports: [CommonModule],
+  templateUrl: './api-client.component.html',
+  styleUrl: './api-client.component.css',
+})
+export class ApiClientComponent {}
